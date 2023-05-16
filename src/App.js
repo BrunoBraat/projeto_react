2cd4d1eb3b01e096cb34b0c2289984fc34332a5b
Bruno Braat <bruno.braat@gmail.com>
2023-05-16 20:12:58 -0300

    Criando um relagio em React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  //variaveis
+  const [hora, setHora] = useState(20);
+  const [minutos, setMinuto] = useState(13);
+  const [segundos, setSegundo] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSegundo(segundos + 1);
+      if (segundos === 59) {
+        setSegundo(0);
+        setMinuto(minutos + 1);
+        if (minutos === 59) {
+          setMinuto(0);
+          setHora(hora + 1);
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className="App">
-      <h2>Olá Mundo</h2>
-    </div>//App
+    <div>
+      <h2 style={{ textAlign: "center" }}>
+        {hora}:{minutos}:{segundos}
+      </h2>
+    </div>
   );
 }
-
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <div className="headerLogo">
+        <h2>Logo empresa</h2>
+      </div>
+      <div className="headerMenu">
+        <a href="#">Home</a>
+        <a href="#">Sobre</a>
+      </div> 
+    </div>//header
+  );
+}
+
